ada84f572529393dc98d4517a0e6f7e90fe25cee
konnor <konnor.frik666@gmail.com>
2024-01-20 22:51:38 +0500

    develop: more test, some fix for old test in case of fix tested func's

--- tests/string_tests/string_tests.h
+++ tests/string_tests/string_tests.h
@@ -1,10 +1,18 @@
 #ifndef __STRING_TESTS_H__
 #define __STRING_TESTS_H__
 
+#define TEST_PRINT 1
+
+#if TEST_PRINT == 1
+    #include <stdio.h>
+#endif
+
 #include <check.h>
 #include <stdio.h>
 #include "../../str_wrap/wrap.h"
 
 Suite* test_string_create();
+Suite* test_str_write();
+Suite* test_string_write();
 
 #endif
