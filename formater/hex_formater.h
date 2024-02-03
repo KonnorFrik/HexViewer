414cf291c3d5d5e5c17335629f42a421e8e353cb
konnor <konnor.frik666@gmail.com>
2024-02-03 23:02:08 +0500

    demo: delete funcs again

--- formater/hex_formater.h
+++ formater/hex_formater.h
@@ -15,18 +15,5 @@ void page_format_destroy(page_format* obj);
  * 1:   obj - NON NULL page_format object ptr for free
  * ret: void */
 
-char decode_symb(uint8_t symb, page_format* obj);
-/* Return symbol for print 
- * If symb non-printable - return default symbol
- * 1:   symb - byte for decode
- * 2:   obj - page_format object ptr
- * ret: decoded - always printable char */
-
-void decode_row(page_format* obj, string* decoded_line);
-/* Decode current n bytes and write in string object
- * 1:   obj - page_format object ptr with bytes
- * 2:   decoded_line - string object ptr for write
- * ret: void */
-
 #endif
 
