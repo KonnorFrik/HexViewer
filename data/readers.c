94a94afb5e6254e7d2725093b7c053c71c974d26
konnor <konnor.frik666@gmail.com>
2024-02-04 00:25:05 +0500

    develop: add todo for readers

--- data/readers.c
+++ data/readers.c
@@ -1,6 +1,7 @@
 #include "readers.h"
 
 size_t read_row(FILE* file, page_format* format_out) {
+    //TODO clear buffer
     size_t readed = fread(format_out->current_row,
                           1,
                           format_out->row_format.bytes_len,
